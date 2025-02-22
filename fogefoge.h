3116948386065e35692ddce92b010e8604385043
oliveira-gu <gu2018oliveirasl@gmail.com>
2025-02-22 09:57:40 -0300

    adding a struct into the code

--- fogefoge.h
+++ fogefoge.h
@@ -1,3 +1,12 @@
 void liberamapa();
 void lemapa();
 void alocamapa();
+void imprimemapa();
+void move(char direcao);
+int acabou();
+
+struct mapa {
+    char** matriz;
+    int linhas;
+    int colunas;
+};
