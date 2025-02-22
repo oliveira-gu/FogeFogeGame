488d6c4c943375e8db482a378bd8eb28038ea8dc
oliveira-gu <gu2018oliveirasl@gmail.com>
2025-02-22 09:23:34 -0300

    formatting the code, and adding the header file  fogefoge.h

--- fogefoge.c
+++ fogefoge.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    // matrix de 5 x 10
-    char mapa[5][11];
+// matrix de 5 x 10
+char** mapa;
+int linhas;
+int colunas;
 
+void liberamapa(){
+    for (int i = 0; i < linhas; i++){
+        free(mapa[i]);
+    }
+    free(mapa);
+}
+
+void alocamapa(){
+    mapa = malloc(sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * (colunas+1));
+    }
+}
+
+void lemapa(){
     FILE* f;
     f = fopen("map.txt", "r");
     if(f ==0) {
@@ -12,16 +28,27 @@ int main(){
         exit(1);
     }
 
+    fscanf(f, "%d %d", &linhas, &colunas);
+
+    alocamapa();
+
     for (int i = 0; i < 5; i++)
     {
         fscanf(f, "%s", mapa[i]);
     }
+
+    fclose(f);
+}
+
+int main(){
+
+    lemapa();
 
     for (int i = 0; i < 5; i++)
     {
         printf("%s\n", mapa[i]);
     }
     
-    fclose(f);
+    liberamapa();
     
 }
